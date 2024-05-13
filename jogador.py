ac1328117986f1d56591c031f028cbd17426980c
Tiago Massuda <137533443+tokujiTO@users.noreply.github.com>
2024-05-13 09:15:23 -0300

    Add files via upload
    COMPONENTES

--- jogador.py
+++ jogador.py
@@ -13,14 +13,18 @@ class jogador():
         self.x = (self.SW - self.JOGADOR_WIDTH) // 2
         self.y = self.SH - self.JOGADOR_HEIGHT - 20
         self.speed = 6
+        self.is_moving = False
+        self.color = "red"
+        self.pontos = 10
 
-    @staticmethod
     def desenhar_jogador(self):
         # TODO adicionar sprite do jogador :3
-        pygame.draw.circle(self.screen, "red", (self.x, self.y), 40)
+        pygame.draw.circle(self.screen, self.color, (self.x, self.y), 40)
 
-    @staticmethod
     def mover_jogador(self, keys):
+        #Posição antiga do jogador:
+        pos_antiga = (self.x, self.y)
+
         # input do usuario
         if keys[pygame.K_LEFT]:
             self.x -= self.speed
@@ -31,6 +35,20 @@ class jogador():
         if keys[pygame.K_DOWN]:
             self.y += self.speed
 
+        #Posição atual do jogador
+        pos_atual = (self.x, self.y)
+
+        if pos_antiga!=pos_atual:
+            self.is_moving = True
+        else:
+            self.is_moving = False
+
+        #mudando de cor se estiver movendo
+        if self.is_moving:
+            self.color = "white"
+        else:
+            self.color = "red"
+
         # garantir que o jogador não saia da tela e não passe da metade de sua altura
         if self.x < 0:
             self.x = 0
@@ -40,6 +58,9 @@ class jogador():
             self.y = self.SH / 2
         elif self.y > self.SH - self.JOGADOR_HEIGHT:
             self.y = self.SH - self.JOGADOR_HEIGHT
+
+    def tira_pontos(self):
+        self.pontos -= 1
 
     def colisao():
         '''
