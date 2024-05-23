5a79b014d2097ca9e0916c06822d0545e93d36cb
Tiago Tokugi de Albuquerque Massuda <tiagomassuda123@gmail.com>
2024-05-23 07:07:15 -0300

    valilda pergunta terminado

--- entities/Pergunta.py
+++ entities/Pergunta.py
@@ -19,6 +19,12 @@ class Pergunta:
             raise ParamNotValidated("alternativas", valida_alternativas[1])
         self.alternativas = alternativas
 
+        valida_resposta_c = self.validate_resposta_correta(resposta_correta)
+        if valida_resposta_c[0] == False:
+            raise ParamNotValidated("resposta correta", valida_resposta_c[1])
+        self.resposta_correta = resposta_correta
+
+
     @staticmethod
     def validate_enunciado(enunciado: str) -> Tuple[bool, str]:
         if enunciado is None:
@@ -26,7 +32,7 @@ class Pergunta:
         if type(enunciado) != str:
             return (False, "O enunciado deve ser uma string")
         return (True, "")
-
+    
     @staticmethod
     def validate_alternativas(alternativas: str) -> Tuple[bool, str]: 
         if alternativas is None:
@@ -35,8 +41,17 @@ class Pergunta:
             return(False, "Devem existir mais de 3 alternativas")
         return (True, "")
     
+    @staticmethod
+    def validate_resposta_correta(resposta_correta: str) -> Tuple[bool,  str]:
+        if type(resposta_correta) != str:
+            return(False, "A resposta correta deve ser uma string")
+        if len(resposta_correta) != 1:
+            return(False, "A resposta correta deve ter apenas um caractere")
+        return(True, "")
+    
     def to_dict(self):
         return {
             "enunciado": self.enunciado,
-            "alternativas": self.alternativas
+            "alternativas": self.alternativas,
+            "resposta_correta": self.resposta_correta
         }
