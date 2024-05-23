8e121a41cb6676284447730ccf2c65a1132bf1ad
Tiago Tokugi de Albuquerque Massuda <tiagomassuda123@gmail.com>
2024-05-23 07:15:59 -0300

    Ajustes nas validaç˜pes

--- entities/Pergunta.py
+++ entities/Pergunta.py
@@ -2,13 +2,14 @@
 from typing import Tuple
 
 from entities.Entity_error import ParamNotValidated
+from enum.answer_type_enum import AnswerTypeEnum
 
 class Pergunta:
     enunciado: str
-    alternativas: str
-    resposta_correta: str
+    alternativas: int
+    resposta_correta: AnswerTypeEnum
 
-    def __init__(self, enunciado: str=None, alternativas: str=None, resposta_correta: str=None):
+    def __init__(self, enunciado: str=None, alternativas: int=None, resposta_correta: AnswerTypeEnum=None):
         valida_enunciado = self.validate_enunciado(enunciado)
         if valida_enunciado[0] == False:
             raise ParamNotValidated("enunciado", valida_enunciado[1])
@@ -34,16 +35,18 @@ class Pergunta:
         return (True, "")
     
     @staticmethod
-    def validate_alternativas(alternativas: str) -> Tuple[bool, str]: 
+    def validate_alternativas(alternativas: int) -> Tuple[bool, str]: 
         if alternativas is None:
             return(False, "Devem existir alternativas")
-        if len(alternativas) <= 3:
+        if alternativas <= 3:
             return(False, "Devem existir mais de 3 alternativas")
         return (True, "")
     
     @staticmethod
-    def validate_resposta_correta(resposta_correta: str) -> Tuple[bool,  str]:
-        if type(resposta_correta) != str:
+    def validate_resposta_correta(resposta_correta: AnswerTypeEnum) -> Tuple[bool,  str]:
+        if resposta_correta is None:
+            return(False, "Selecione uma resposta correta")
+        if type(resposta_correta) != AnswerTypeEnum:
             return(False, "A resposta correta deve ser uma string")
         if len(resposta_correta) != 1:
             return(False, "A resposta correta deve ter apenas um caractere")
---
+++ enum/answer_type_enum.py
@@ -0,0 +1,9 @@
+
+from enum import Enum
+
+class AnswerTypeEnum(Enum):
+    a="A"
+    b="B"
+    c="C"
+    d="D"
+    e="E"
